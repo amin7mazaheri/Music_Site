d02926b091927ddab48bfe01be60e3e360ecb4c9
amin <mazaheri7amin@gmail.com>
2019-01-04 15:59:35 -0500

    a bit change in the rating

--- app_music/templatetags/custom_datetime.py
+++ app_music/templatetags/custom_datetime.py
@@ -9,13 +9,15 @@ def date_spiliter(date):
 	# return date.strftime('%Y-%m-%d') 
 @register.filter
 def rating (rate):
-	html = ''
+	html = '<fieldset class="rating">'
 	for i in range (5):
 		active = None
 		if rate > i:
 			acitve = style = 'color:#ffd700'
 		html +=f'''
-			 <input type="radio" id="star{i}" name="rating" value="1" /><label class = "full" {active}for="star1" title="Sucks big time - 1 star"></label>
-    <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" {active}for="starhalf" title="Sucks big time - 0.5 stars"></label>
+			 <input type="radio" id="star{i}" name="rating" value="{i}" /><label class = "full" {active}for="star{i}" title="Sucks big time - {i} star"></label>
+    <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" {active} for="starhalf" title="Sucks big time - 0.5 stars"></label>
+
 			'''
+	html += '</fieldset>'
 	return html 
